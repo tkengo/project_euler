86c39b1a4a2146aeb8169a227643c3d7cd13babc
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2015-07-31 12:10:29 +0900

    Implemented more efficiently method for fibonacci

--- 002_fibonacci.rb
+++ 002_fibonacci.rb
@@ -1,12 +1,34 @@
+N = 4000000
+
+# -----------------------
+# Normal method
+# -----------------------
 def fibonacci(n1, n2, sum)
   n3 = n1 + n2
-  puts n3
-  sum += n3 if n3 % 2 == 0
-  if n3 > 4000000
+  sum += n3 if n3.even?
+  if n3 > N
     sum
   else
     fibonacci(n2, n3, sum)
   end
 end
 
-puts fibonacci(1, 1, 0)
+puts fibonacci(0, 1, 0)
+
+# -----------------------
+# More efficiently method
+# -----------------------
+def fibonacci_efficiently(n1, n2, sum)
+  n3 = n1 + n2
+  n4 = n2 + n3
+  n5 = n3 + n4
+
+  if n4 > N
+    sum
+  else
+    sum += n4
+    fibonacci_efficiently(n4, n5, sum)
+  end
+end
+
+puts fibonacci_efficiently(0, 1, 0)
